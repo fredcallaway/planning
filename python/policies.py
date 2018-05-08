a3ed90d51e4d1c4ada29cc3edc79b1bfbfe743ef
fredcallaway <fredc@llaway.com>
2018-05-08 13:24:50 -0700

    clean up SoftmaxPolicy

--- python/policies.py
+++ python/policies.py
@@ -66,10 +66,7 @@ class SoftmaxPolicy(Policy):
         return np.random.choice(len(probs), p=probs)
 
     def action_distribution(self, state):
-        q = np.zeros(self.n_action) - 1e30
-        for a in self.env.actions(state):
-            q[a] = self.preference(state, a)
-        return softmax(q, self.temp)
+        return softmax(self.preferences(state), self.temp)
 
     def preferences(self, state):
         q = np.zeros(self.n_action) - 1e30
